c4a2f4055357a34252e6b1e1c1f32d17b3e7b928
GuangjieYu1 <118854250+GuangjieYu1@users.noreply.github.com>
2026-07-12 18:33:05 +0800

    feat: register history and comparison routes

--- py-src/data_formulator/insight/routes/__init__.py
+++ py-src/data_formulator/insight/routes/__init__.py
@@ -2,5 +2,6 @@ from .health import insight_health_bp
 from .project import insight_project_bp
 from . import version_profiles as _version_profiles
 from . import output_analysis as _output_analysis
+from . import history as _history
 
 __all__ = ["insight_health_bp", "insight_project_bp"]
